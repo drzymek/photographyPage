9c463fa99d046b1e511e655687099c156bbe40ab
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-03-06 00:42:07 +0000

    create components in pages folder and add to Page.js

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import AboutPage from '../pages/AboutPage';
+import GalleryPage from '../pages/GalleryPage';
+import StoriesPage from '../pages/StoriesPage';
+import ContactPage from '../pages/ContactPage';
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/gallery" component={GalleryPage} />
+            <Route path="/stories" component={StoriesPage} />
+            <Route path="/contact" component={ContactPage} />
+        </>
     );
 }
 
---
+++ src/pages/StoriesPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StoriesPage = () => {
+    return (
+        <div>Stories</div>
+    );
+}
+
+export default StoriesPage;
