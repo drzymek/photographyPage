026845005684a11286db6d95231170b486d5289d
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-04-17 23:58:04 +0100

    add Route to component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import '../css/SingleGallery.css'
+
+import KidsGallery from '../galeries/KidsGallery';
+import NewbornGallery from '../galeries/NewbornGallery';
+import PregnancyGallery from '../galeries/PregnancyGallery';
+import PortraitsGallery from '../galeries/PortraitsGaller';
+import FamiliesGallery from '../galeries/FamiliesGallery';
+
 
 const Gallery = (props) => {
 
     return (
-        <div>
+        <div className="allGaleries">
             <h2>{props.id}</h2>
-            <div>galeria</div>
+            <Route path="/galeria/niemowlaki" component={NewbornGallery} />
+            <Route path="/galeria/ciąża" component={PregnancyGallery} />
+            <Route path="/galeria/dzieci" component={KidsGallery} />
+            <Route path="/galeria/portrety" component={PortraitsGallery} />
+            <Route path="/galeria/rodziny" component={FamiliesGallery} />
         </div>
     );
 }
