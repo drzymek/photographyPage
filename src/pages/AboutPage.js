2f34cbfff894911d1736681fde808cda682157aa
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-06-12 01:14:30 +0100

    remove div class="content"

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,20 +7,18 @@ import { Link } from 'react-router-dom';
 const AboutPage = () => {
     return (
         <div className="about navScrollToPage">
-            <div className="content">
-                <div className="photo">
-                    <img src={ownerPhoto} alt="page owner Kamila" />
-                    <img src={photoAbout} alt="little girl in the grass" />
-                </div>
-                <div className="welcome">
-                    <h1>Hej! Miło mi Cię widzieć!</h1>
-                    <p>Mam na imię Kamila, uwielbiam fotografię i zawsze staram sie opowiadać historię moimi zdjęciami..., uwieczniać momenty, które będzie miło wspominać zarówno teraz jak i po latach. Po prostu utrwalam to co najpiękniejsze.</p>
-                    <br></br>
-                    <p>Zapraszam do obejrzenia mojej galerii.</p>
-                    <button>
-                        <Link to="/galerie">Galeria</Link>
-                    </button>
-                </div>
+            <div className="photo">
+                <img src={ownerPhoto} alt="page owner Kamila" />
+                <img src={photoAbout} alt="little girl in the grass" />
+            </div>
+            <div className="welcome">
+                <h1>Hej! Miło mi Cię widzieć!</h1>
+                <p>Mam na imię Kamila, uwielbiam fotografię i zawsze staram sie opowiadać historię moimi zdjęciami..., uwieczniać momenty, które będzie miło wspominać zarówno teraz jak i po latach. Po prostu utrwalam to co najpiękniejsze.</p>
+                <br></br>
+                <p>Zapraszam do obejrzenia mojej galerii.</p>
+                <button>
+                    <Link to="/galerie">Galeria</Link>
+                </button>
             </div>
         </div>
     );
