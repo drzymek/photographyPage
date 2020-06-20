066933296f6d0597b81766e51314c16e46625463
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2020-06-20 01:22:40 +0100

    update styles for StoriesPage.js

--- src/pages/StoriesPage.js
+++ src/pages/StoriesPage.js
@@ -9,11 +9,11 @@ const StoriesPage = () => {
             <div className="videos">
                 <div className="video">
                     <h3>summer 2018</h3>
-                    <iframe src="https://player.vimeo.com/video/284954848" width="512" height="288" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title="summer 2018"></iframe>
+                    <iframe src="https://player.vimeo.com/video/284954848" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title="summer 2018"></iframe>
                 </div>
                 <div className="video">
                     <h3>tales of summer</h3>
-                    <iframe src="https://player.vimeo.com/video/276782608" width="512" height="288" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title="tales of summer"></iframe>
+                    <iframe src="https://player.vimeo.com/video/276782608" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title="tales of summer"></iframe>
                 </div>
             </div>
         </div>
