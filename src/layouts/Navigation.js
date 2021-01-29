734fed87eb4875481b79862dd1c8a5d1693d0cdd
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-29 13:26:29 +0100

    add newLogo img to navigation.js

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import "../css/Navigation.css";
-import imgLogo from "../images/logo.jpg"
+import imgLogo from "../images/newLogo.jpg"
 
 const menuList = [
     { name: "strona główna", path: "/", exact: true },
