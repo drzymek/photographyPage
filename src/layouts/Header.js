f65715335f2a53f803cbfef54fa88f60bfe077ad
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-30 00:22:59 +0100

    delete image from Header.js

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -6,9 +6,10 @@ import img02 from '../images/header_photos/img02.jpg';
 import img03 from '../images/header_photos/img03.jpg';
 import img04 from '../images/header_photos/img04.jpg';
 import img05 from '../images/header_photos/img05.jpg';
+import img06 from '../images/header_photos/img06.jpg';
 
 const Header = () => {
-    const headerImages = [img01, img02, img03, img04, img05];
+    const headerImages = [img01, img02, img03, img04, img05, img06];
     const imageIndex = Math.floor(Math.random() * headerImages.length);
     const photo = headerImages[imageIndex];
     return (
