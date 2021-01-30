c70ec4a914f601790c46fa4d319fa5f14bf08fcb
Tomasz Drzymala <tomaszdrzymala.pl@gmail.com>
2021-01-30 20:03:53 +0100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './layouts/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
